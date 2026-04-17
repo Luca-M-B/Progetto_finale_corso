40ef25e3b59e3a2ce48546f7ac64cb907be23517
LucaMB <lmbaratti@gmail.com>
2026-04-17 12:12:25 +0200

    application.yaml + pom

---
+++ progetto_parking_system/src/main/java/com/example/progetto_parking_system/ProgettoParkingSystemApplication.java
@@ -0,0 +1,15 @@
+package com.example.progetto_parking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoParkingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoParkingSystemApplication.class, args);
+
+		System.out.println("------ PROGRAMMA RUNNATO ------");
+	}
+
+}
