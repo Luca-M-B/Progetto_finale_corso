8eea5c4a4291bb359dd44dac79de21cf086a5e7b
LucaMB <lmbaratti@gmail.com>
2026-04-17 12:39:27 +0200

    struttura completa

---
+++ progetto_parking_system/src/main/java/com/example/progetto_parking_system/enums/SpotType.java
@@ -0,0 +1,5 @@
+package com.example.progetto_parking_system.enums;
+
+public class SpotType {
+
+}
---
+++ progetto_parking_system/src/main/java/com/example/progetto_parking_system/service/ReservationService.java
@@ -0,0 +1,5 @@
+package com.example.progetto_parking_system.service;
+
+public class ReservationService {
+
+}
