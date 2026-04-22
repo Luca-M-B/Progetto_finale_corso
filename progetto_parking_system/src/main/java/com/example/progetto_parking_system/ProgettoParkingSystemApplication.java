0714252b4bf1a8b2fe108bd16fbec56ea7fe802f
LucaMB <lmbaratti@gmail.com>
2026-04-22 15:44:19 +0200

    sistemato orario - time zone

--- progetto_parking_system/src/main/java/com/example/progetto_parking_system/ProgettoParkingSystemApplication.java
+++ progetto_parking_system/src/main/java/com/example/progetto_parking_system/ProgettoParkingSystemApplication.java
@@ -3,8 +3,18 @@ package com.example.progetto_parking_system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ProgettoParkingSystemApplication {
+
+	@PostConstruct
+	public void init() {
+		// Imposta la timezone predefinita per l'applicazione
+		TimeZone.setDefault(TimeZone.getTimeZone("Europe/Rome"));
+		System.out.println("Timezone impostata a: " + TimeZone.getDefault().getID());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProgettoParkingSystemApplication.class, args);
