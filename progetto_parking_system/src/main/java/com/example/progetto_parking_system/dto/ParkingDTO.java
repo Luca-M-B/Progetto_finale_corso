0ed7f7856c5172b4753f5154dc069edd8682e07b
MazzottaLuca <lucamazzotta99@gmail.com>
2026-04-23 16:55:16 +0200

    programma runnato 11

--- progetto_parking_system/src/main/java/com/example/progetto_parking_system/dto/ParkingDTO.java
+++ progetto_parking_system/src/main/java/com/example/progetto_parking_system/dto/ParkingDTO.java
@@ -1,5 +1,15 @@
 package com.example.progetto_parking_system.dto;
 
-public class ParkingDTO {
+import java.util.List;
+import lombok.Data;
 
+@Data
+public class ParkingDTO {
+    private Long id;
+    private String name;
+    private String address;
+    private double maxCapacity;
+    private double currentOccupancy;
+    private double hourlyRate;
+    private List<SpotDTO> spots;
 }
